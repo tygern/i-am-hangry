0be321e9f5c8de00b36179d8352ce58419a58636
tygern <tygern@gmail.com>
2014-03-09 13:32:47 -0600

    return data from factual

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source "https://rubygems.org"
 ruby "2.1.1"
 
 gem "sinatra"
-gem "httparty"
+gem "oauth"
--- lib/hangry/hangry.rb
+++ lib/hangry/hangry.rb
@@ -1,7 +1,24 @@
 require "sinatra"
+require "oauth"
+require "yaml"
+require "json"
 
 class Hangry < Sinatra::Base
   get "/" do
-    "hello"
+    content_type :json
+
+    authentication = YAML.load_file(File.expand_path("../../config/access.yml", __dir__))
+    consumer = OAuth::Consumer.new authentication["key"],
+                                 authentication["secret"],
+                                 {site: authentication["site"]}
+    token = OAuth::AccessToken.new(consumer)
+
+    response = token.get("http://api.v3.factual.com/t/restaurants-us?q=Boulder")
+
+    restaurants = JSON.parse(response.body)["response"]["data"].inject([]) do |list, place|
+      list << {name: place["name"], tags: place["cuisine"]}
+    end
+
+    restaurants.to_json
   end
 end
